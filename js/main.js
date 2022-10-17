fb05ea6c708ce443e4151da70c39c503c800f2fd
Roberto Ciccarelli <robyciccarelli.rc@gmail.com>
2022-10-17 21:21:25 +0200

    FIx accordion

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 $.fn.nextOrFirst = function(selector) {
   var next = this.next(selector);
-  return (next.length) ? next : this.prevAll(selector).last();
+  return(next.length) ? next : this.prevAll(selector).last();
+}
+
+$.fn.accordion = function (cssclass) {
+  var container = $(this);
+  $(cssclass+"-header", this).on("click", function () {
+    parent = $(this).parent();
+    elem = $(cssclass+"-content", parent);
+    if (!parent.hasClass("active")) {
+      $(container).removeClass("active");
+      $(parent).addClass("active");
+    } else {
+      $(parent).removeClass("active");
+    }
+  });
+
 }
 
 $(document).ready(function () {
@@ -24,16 +39,8 @@ $(document).ready(function () {
 
   }, 15000);
 
-  $(".accordion li .item-header").on("click", function () {
-    parent = $(this).parent();
-    elem = $(".item-content", parent);
-    if (!parent.hasClass("active")) {
-      $(".accordion li").removeClass("active");
-      $(parent).addClass("active");
-    } else {
-      $(parent).removeClass("active");
-    }
-  });
+  $(".column.small-collapse").accordion(".column");
+  $(".accordion li").accordion(".item");
 
 
 });
